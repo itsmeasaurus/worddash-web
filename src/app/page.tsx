3c6341aace22572ba64a5ec1d993b49a0953bdff
Htet Wai Yan Aung <htetwaiyanaung.acc2@gmail.com>
2026-03-04 23:40:08 +0700

    add analytics tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import { HeroSection } from "@/components/hero-section";
 import { HowItWorks } from "@/components/how-it-works";
 import { PricingSection } from "@/components/pricing-section";
 import { FooterCta } from "@/components/footer-cta";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function HomePage() {
   return (
+    <>
+    <Analytics />
     <main className="min-h-screen bg-[#F5E642]">
       <Navbar />
       <HeroSection />
@@ -13,5 +16,6 @@ export default function HomePage() {
       <PricingSection />
       <FooterCta />
     </main>
+    </>
   );
 }
